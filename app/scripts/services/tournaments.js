22a02a1aa8ec2dc5f9fa4b0129964250249fe9c5
beholderrk <behoderrk@gmail.com>
2014-08-07 20:08:07 +0400

    remove tournament players action, players now as tournament field

--- app/scripts/services/tournaments.js
+++ app/scripts/services/tournaments.js
@@ -1,7 +1,5 @@
 'use strict';
 
 angular.module('chessTourFrontApp').factory('Tournament', function (djResource) {
-    return djResource('/api/tournaments/:id/:action', {id: '@id'}, {
-        players: {method: 'GET', params: {action: 'players'}, isArray: true}
-    });
+    return djResource('/api/tournaments/:id/:action', {id: '@id'});
 });
